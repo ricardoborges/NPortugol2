a00d8b96a7c8cc5eae24464e23481af5215a2f6e
Ricardo Borges <ricardoborges@gmail.com>
2012-10-12 23:22:54 -0300

    new parser

---
+++ src/NPortugol2/VirtualMachine/Instruction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace NPortugol2.VirtualMachine
+{
+    public class Instruction
+    {
+        public OpCode OpCode { get; set; }
+
+        public Type DeclaringType { get; set; }
+
+        public bool IsLocal { get; set; }
+
+        public string[] Operands { get; set; }
+
+        public object Value { get; set; }
+
+        public int IntValue
+        {
+            get { return int.Parse(Value.ToString()); }
+        }
+
+        public float FloatValue
+        {
+            get { return float.Parse(Value.ToString()); }
+        }
+    }
+}
